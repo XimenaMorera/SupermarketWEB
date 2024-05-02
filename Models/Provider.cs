d6b9d8a174a03f5c5c2a51814b80635bbe5dfd9f
XimenaMorera <moreraximena@36@gmail.com>
2024-05-02 18:30:32 -0500

    Delete provider

--- Models/Provider.cs
+++ Models/Provider.cs
@@ -2,5 +2,10 @@
 {
 	public class Provider
 	{
+		public int Id { get; set; } //llave primaria
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
 	}
 }
---
+++ Pages/Providers/Delete.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Providers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+
+        public Provider Provider { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+            var provider = await _context.Providers.FirstOrDefaultAsync(m => m.Id == id);
+            if (provider == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Provider = provider;
+                _context.Providers.Remove(provider);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
