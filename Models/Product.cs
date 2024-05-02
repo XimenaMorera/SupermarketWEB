3db2e7fce96f2cbadaa341868903f4a1112a715a
XimenaMorera <moreraximena@36@gmail.com>
2024-05-02 13:16:05 -0500

    Delete Product

--- Models/Product.cs
+++ Models/Product.cs
@@ -15,8 +15,8 @@ namespace SupermarketWEB.Models
 
         public int CategoryId { get; set; }//serla la llave foranea
 
-        public Category Category { get; set; }//propiedad de navegacion
+        public Category Category { get; set; } = default!;//propiedad de navegacion
 
 
-    }
+	}
 }
