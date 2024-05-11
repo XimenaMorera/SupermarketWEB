b176d2270f2d04abaf3aa098b8fabfa840b6b264
XimenaMorera <moreraximena@36@gmail.com>
2024-05-11 13:12:01 -0500

    Delete User

---
+++ Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+		private readonly SupermarketContext _context;
+		public DeleteModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+		[BindProperty]
+
+		public User User { get; set; } = default!;
+
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null || _context.Users == null)
+			{
+				return NotFound();
+			}
+			var user = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				User = user;
+				_context.Users.Remove(user);
+				await _context.SaveChangesAsync();
+			}
+			return RedirectToPage("./Index");
+		}
+
+	}
+}
