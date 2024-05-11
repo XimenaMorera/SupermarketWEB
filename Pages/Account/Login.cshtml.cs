e15e686b85330af97d71004899773c61e8c413e4
XimenaMorera <moreraximena@36@gmail.com>
2024-05-11 12:56:10 -0500

    Cambios en login

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,25 +1,39 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
 using SupermarketWEB.Models;
 using System.Security.Claims;
 
 namespace SupermarketWEB.Pages.Account
 {
     public class LoginModel : PageModel
-    {
-        [BindProperty]
 
-        public User User { get; set; }
+    {
+		private readonly SupermarketContext _context;
+
+		[BindProperty]
+		public User User { get; set; }
+
+		public LoginModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+		
         public void OnGet()
         {
         }
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid) return Page();
-            if (User.Email == "" && User.Password == "")
-            { //creando los claim, datos a almacenar en la cookie
-                var claims = new List<Claim>
+			if (!ModelState.IsValid) return Page();
+
+			// Busca un usuario con el correo electrónico especificado
+			var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == User.Email);
+
+			if (user != null && user.Password == User.Password)
+            { 
+				var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Name, "admin"),
                     new Claim(ClaimTypes.Email,User.Email),
@@ -33,8 +47,9 @@ namespace SupermarketWEB.Pages.Account
                 await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
                 return RedirectToPage("/index");
             }
-            return Page();
-        }
+			ModelState.AddModelError(string.Empty, "Correo electrónico o contraseña incorrectos.");
+			return Page();
+		}
 
     }
 }
